c5c335b51b7afeee5048305d9707906844ea1a5d
Richard Harris <richard@idestini.com>
2019-05-13 16:47:25 -0500

    Made physical board with checkboxes

--- nonogram.js
+++ nonogram.js
@@ -1,11 +1,21 @@
 var spaces = [];
+var nono = document.getElementById("nono");
+var board = document.createElement("table");
 
 for(x = 1; x <= 10; x++) {
-    var row = [];
-    for(y = 1; y <= 10; y++) {
-        row.push(Math.floor(Math.random() * Math.floor(2)));
-    }
-    spaces.push(row);
-}
+    //var row = [];
+    var row = document.createElement("tr");
 
-console.log(spaces);
+    for(y = 1; y <= 10; y++) {
+        var space = document.createElement("td");
+        var check = document.createElement("input");
+        check.type = "checkbox";
+        
+        check.checked = Math.floor(Math.random() * Math.floor(2)) == 1 ? true : false;
+        space.appendChild(check);
+        row.appendChild(space);
+    }
+    //spaces.push(row);
+    board.appendChild(row);
+}
+nono.appendChild(board);
