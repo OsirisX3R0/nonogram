c88e6f4c3859bd895c650d621c5e08efb24de80d
Richard Harris <richardharris916@gmail.com>
2022-03-17 01:23:41 -0500

    Added BoardStateEnum

---
+++ src/enums/BoardStateEnum.js
@@ -0,0 +1,7 @@
+module.exports = {
+  GENERATING: 0,
+  GENERATED: 1,
+  STARTED: 2,
+  COMPLETE: 3,
+  FAILED: 4,
+};
---
+++ src/enums/types/BoardStateEnum.d.ts
@@ -0,0 +1,14 @@
+enum BoardStateEnum  {
+  /** The board is being built */
+  GENERATING,
+  /** The board has been built */
+  GENERATED,
+  /** The game has begun */
+  STARTED,
+  /** The game has been won */
+  COMPLETE,
+  /** The game has been lost */
+  FAILED
+}
+
+export default BoardStateEnum
