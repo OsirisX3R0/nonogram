4e8413619a851b421450f092ed94e3cbc4298bfc
Richard Harris <richardharris916@gmail.com>
2022-03-19 23:09:24 -0500

    Added BoardStateEnum to main exports

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ const Board = require("./src/board");
 const Tile = require("./src/tile");
 const TileFlaggedEnum = require("./src/enums/TileFlaggedEnum");
 const TileStateEnum = require("./src/enums/TileStateEnum");
+const BoardStateEnum = require("./src/enums/BoardStateEnum");
 
-module.exports = { Board, Tile, TileFlaggedEnum, TileStateEnum };
+module.exports = { Board, Tile, TileFlaggedEnum, TileStateEnum, BoardStateEnum };
